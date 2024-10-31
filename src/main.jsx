3be6247e3cc29de9f9481598b5078b8ff6b1066f
guyzon777@gmail.com <guyzon777@gmail.com>
2024-10-31 06:16:20 +0545

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+createRoot(document.getElementById('root')).render(
+
+  // <React.StrictMode>
+
+  //   <BrowserRouter basename='/'>
+  //   <Routes>
+  //     <Route path='*' element={<App/>}/>
+  //   </Routes>
+  //   </BrowserRouter>
+  //   {/* , */}
+  // </React.StrictMode>
+  
+
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
